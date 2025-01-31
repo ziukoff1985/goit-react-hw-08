1e7da9d3fc0dca8b8b47203046d07701518e78a1
Borys Ziukov <ziukoff1985@gmail.com>
2025-01-31 10:44:58 +0200

    added AppBar styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-
 import { Provider } from 'react-redux';
-
 import { store } from './redux/store.js';
 import { BrowserRouter } from 'react-router-dom';
+import 'modern-normalize';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
